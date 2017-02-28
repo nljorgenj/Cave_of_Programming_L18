562260c6c7d10828e4a7f5d5d7fa4a5e8c1e0b03
nljorgenj <nljorgenj@gmail.com>
2017-02-28 22:00:49 +0100

    while loops

---
+++ while-loops/while.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int i = 0;
+
+	while (i<5) {
+		cout << "hello" << endl;
+		i++;
+	}
+
+	cout << "Programming quitting." << endl;
+
+	return 0;
+}
